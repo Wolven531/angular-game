d463e42c97cbeee7db51a47811ae0fac5f1afe16
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-18 18:45:47 -0700

    Add logic for if coins are too few

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,17 @@ export class AppComponent {
 	private coins = 0
 	private goldBars = 0
 
-	private onGenerateCoin(evt) {
-		this.coins += 1
-	}
-
 	private onExchangeCoinsForBar(evt) {
+		if (this.coins < 10) {
+			alert(`Insufficient coins, need at least ${10}`)
+			return
+		}
+
 		this.coins -= 10
 		this.goldBars += 1
+	}
+
+	private onGenerateCoin(evt) {
+		this.coins += 1
 	}
 }
