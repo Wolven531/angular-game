95526e979823de10181b9c3551949ab79562af6b
Wolven531 <anthony.williams.cs@gmail.com>
2020-01-20 21:37:54 -0700

    Add Welcome component to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-// import { AppComponent } from './app.component'
+import { WelcomeComponent } from '../welcome/welcome.component'
 
 const routes: Routes = [
 	// NOTE: the root doesn't need mapped if there are no others -
 	//   mapping it anyways seems to duplicate `ag-root`
-	// { path: '', component: AppComponent }
+	{ path: 'welcome', component: WelcomeComponent },
+	{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
+	{ path: '**', redirectTo: 'welcome', pathMatch: 'full' }
 ]
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [
+		RouterModule.forRoot(routes)
+	],
 	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core'
-import { FormsModule }    from '@angular/forms'
+import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
+import { WelcomeComponent } from '../welcome/welcome.component'
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		WelcomeComponent
 	],
 	imports: [
 		BrowserModule,
