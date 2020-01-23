d5c5cbbe8d437503cb43c89d3bd791ddf24cf240
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-23 16:02:40 -0700

    Fix game component styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,4 @@ import { Component } from '@angular/core'
 	templateUrl: './app.component.html'
 })
 export class AppComponent {
-	private coins = 0
-	private goldBars = 0
-
-	private onExchangeCoinsForBar(evt) {
-		if (this.coins < 10) {
-			alert(`Insufficient coins, need at least ${10}`)
-			return
-		}
-
-		this.coins -= 10
-		this.goldBars += 1
-	}
-
-	private onGenerateCoin(evt) {
-		this.coins += 1
-	}
 }
--- src/game/game.component.ts
+++ src/game/game.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'ag-game',
   styleUrls: ['./game.component.scss'],
   templateUrl: './game.component.html'
 })
-export class GameComponent implements OnInit {
+export class GameComponent {
+	private coins = 0
+	private goldBars = 0
 
-  constructor() { }
+	private onExchangeCoinsForBar(evt) {
+		if (this.coins < 10) {
+			alert(`Insufficient coins, need at least ${10}`)
+			return
+		}
 
-  ngOnInit() {
-  }
+		this.coins -= 10
+		this.goldBars += 1
+	}
 
+	private onGenerateCoin(evt) {
+		this.coins += 1
+	}
 }
