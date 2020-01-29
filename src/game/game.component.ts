a38bb47f2ce4e4d973e67291cb320b2ca1952e1b
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-29 14:48:44 -0700

    First pass at loading localStorage onInit

--- src/game/game.component.ts
+++ src/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 // NOTE: rate vs. click - value of 1 equals "1 to 1"; this means
 // means one click generates one coin
@@ -14,11 +14,24 @@ const STARTING_GOLD_BARS = 0
 	styleUrls: ['./game.component.scss'],
 	templateUrl: './game.component.html'
 })
-export class GameComponent {
-	// const loadedCoins = parseInt(window.localStorage.getItem('coins'), 10)
-	// const loadedGoldBars = parseInt(window.localStorage.getItem('goldBars'), 10)
-	private coins = STARTING_COINS
-	private goldBars = STARTING_GOLD_BARS
+export class GameComponent implements OnInit {
+	private coins = 0
+	private goldBars = 0
+
+	public ngOnInit() {
+		const loadedCoins = parseInt(window.localStorage.getItem('coins'), 10)
+		const loadedGoldBars = parseInt(window.localStorage.getItem('goldBars'), 10)
+
+		if (isNaN(loadedCoins)) {
+			this.coins = STARTING_COINS
+			window.localStorage.setItem('coins', String(this.coins))
+		}
+
+		if (isNaN(loadedGoldBars)) {
+			this.goldBars = STARTING_GOLD_BARS
+			window.localStorage.setItem('goldBars', String(this.goldBars))
+		}
+	}
 
 	private onExchangeCoinsForBar(evt) {
 		const cost = Math.round(1 / EXCHANGE_RATE_GOLD_BAR)
