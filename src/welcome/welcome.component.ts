ae24a17a1d2ae4cfce7e0bc6724ec346eef77fca
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-30 09:04:43 -0700

    Add basic style for Welcome component

--- src/welcome/welcome.component.ts
+++ src/welcome/welcome.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
 	// selector: 'ag-welcome',
+	styleUrls: ['./welcome.component.scss'],
 	templateUrl: './welcome.component.html'
 })
 export class WelcomeComponent {
