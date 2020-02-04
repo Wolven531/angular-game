d9dbe9df204999fefe035676a1e376839efa4bda
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-03 22:13:20 -0700

    Add style for minion component

--- src/game/minion.component.ts
+++ src/game/minion.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core'
 
 @Component({
 	selector: 'ag-minion',
-	// styleUrls: ['./minion.component.scss'],
+	styleUrls: ['./minion.component.scss'],
 	templateUrl: './minion.component.html'
 })
 export class MinionComponent {
