e34f240a9c730b7fc947c6ce415ace71c4c8e2c9
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-04 20:58:27 -0700

    Rename unit test files, update shop unit tests

---
+++ src/shop/shop.component.unit.spec.ts
@@ -0,0 +1,77 @@
+import {
+	async,
+	ComponentFixture,
+	TestBed
+} from '@angular/core/testing'
+
+import { ShopComponent } from './shop.component'
+
+describe('ShopComponent', () => {
+	beforeEach(
+		async(() => {
+			TestBed.configureTestingModule({
+				declarations: [ ShopComponent ]
+			})
+			.compileComponents();
+		})
+	)
+
+	describe('when created', () => {
+		let compiled: HTMLElement
+		let fixture: ComponentFixture<ShopComponent>
+
+		beforeEach(() => {
+			fixture = TestBed.createComponent(ShopComponent)
+			compiled = fixture.debugElement.nativeElement
+		})
+
+		it('creates shop component', () => {
+			expect(fixture.debugElement.componentInstance).toBeTruthy()
+		})
+
+		it('renders headers', () => {
+			expect(compiled.querySelector('.shop-container > h3:nth-of-type(1)').textContent).toBe('Shop')
+			expect(compiled.querySelector('.shop-container > h3.coins').textContent).toBe('Num coins: ')
+			expect(compiled.querySelector('.shop-container > h3.gold')).toBeNull()
+		})
+
+		it('renders buttons', () => {
+			const buttons = compiled.querySelectorAll('.shop-container > button')
+			expect(buttons.length).toBe(2)
+
+			expect(buttons.item(0).textContent).toBe('Exchange 10 coins for gold bar')
+			expect(buttons.item(0).classList.contains('btn-exchange-coins')).toBe(true)
+
+			expect(buttons.item(1).textContent).toBe('Exchange 1 gold bar for 10 coins')
+			expect(buttons.item(1).classList.contains('btn-exchange-bars')).toBe(true)
+		})
+
+		describe('when changes are detected', () => {
+			beforeEach(() => {
+				fixture.detectChanges()
+			})
+
+			it('hides conditional header', () => {
+				expect(compiled.querySelector('.shop-container > h3.gold')).toBeNull()
+			})
+
+			it('renders hydrated header (w/ value)', () => {
+				expect(compiled.querySelector('.shop-container > h3.coins').textContent).toBe('Num coins: 0')
+				expect(compiled.querySelector('.shop-container > h3.coins > .value').textContent).toBe('0')
+			})
+
+			it('renders hydrated buttons (w/ possible disabled attribute)', () => {
+				const buttons = compiled.querySelectorAll('.shop-container > button')
+				expect(buttons.length).toBe(2)
+
+				const exchangeCoinsDisabledAtt = buttons.item(0).getAttributeNode('disabled')
+				expect(exchangeCoinsDisabledAtt).not.toBeNull()
+				expect(exchangeCoinsDisabledAtt.value).toBe('')
+
+				const exchangeBarsDisabledAtt = buttons.item(1).getAttributeNode('disabled')
+				expect(exchangeBarsDisabledAtt).not.toBeNull()
+				expect(exchangeBarsDisabledAtt.value).toBe('')
+			})
+		})
+	})
+})
