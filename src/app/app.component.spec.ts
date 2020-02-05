2b326ac476d7e2d964d6768ed5e8e98e412b2e77
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-04 20:03:20 -0700

    Add unit test for header in app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,21 @@
 import { async, TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
+
 import { AppComponent } from './app.component'
 
 describe('AppComponent', () => {
-	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [
-				AppComponent
-			],
-			imports: [
-				RouterTestingModule
-			]
-		}).compileComponents()
-	}))
+	beforeEach(
+		async(() => {
+			TestBed.configureTestingModule({
+				declarations: [
+					AppComponent
+				],
+				imports: [
+					RouterTestingModule
+				]
+			}).compileComponents()
+		})
+	)
 
 	it('should create the app', () => {
 		const fixture = TestBed.createComponent(AppComponent)
@@ -20,16 +23,17 @@ describe('AppComponent', () => {
 		expect(app).toBeTruthy()
 	})
 
-	it(`should have as title 'angular-game'`, () => {
-		const fixture = TestBed.createComponent(AppComponent)
-		const app = fixture.debugElement.componentInstance
-		expect(app.title).toEqual('angular-game')
-	})
+	// it(`should have as title 'angular-game'`, () => {
+	// 	const fixture = TestBed.createComponent(AppComponent)
+	// 	const app = fixture.debugElement.componentInstance
+	// 	expect(app.title).toEqual('angular-game')
+	// })
 
-	it('should render title', () => {
+	it('should render header', () => {
 		const fixture = TestBed.createComponent(AppComponent)
 		fixture.detectChanges()
-		const compiled = fixture.debugElement.nativeElement
-		expect(compiled.querySelector('.content span').textContent).toContain('angular-game app is running!')
+
+		const compiled: HTMLElement = fixture.debugElement.nativeElement
+		expect(compiled.querySelectorAll('.container > .header > nav li').length).toBe(3)
 	})
 })
