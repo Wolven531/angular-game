4921fbac7dad2dba09fe10503809221b85e73bcc
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-06 20:40:04 -0700

    Export Routes from game and shop modules

--- src/shop/shop.module.ts
+++ src/shop/shop.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { RouterModule } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router'
 
 import { ShopComponent } from './shop.component'
 
-const routes = [
+export const routes: Routes = [
 	{ path: 'shop', component: ShopComponent }
 ]
 
