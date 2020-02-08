fbd10184a4a94d6429981637def53f79c52a6e64
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-07 20:17:27 -0700

    Attempt to use PhantomJS from NPM

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
 	config.set({
 		autoWatch: true,
 		basePath: '',
-		browsers: ['Chrome'],
+		browsers: ['PhantomJS'],
 		client: {
 			clearContext: false // leave Jasmine Spec Runner output visible in browser
 		},
@@ -17,9 +17,14 @@ module.exports = function (config) {
 		},
 		frameworks: ['jasmine', '@angular-devkit/build-angular'],
 		logLevel: config.LOG_INFO,
+		phantomjsLauncher: {
+			// Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+			exitOnResourceError: true
+		},
 		plugins: [
 			require('karma-jasmine'),
-			require('karma-chrome-launcher'),
+			// require('karma-chrome-launcher'),
+			require('karma-phantomjs-launcher'),
 			require('karma-jasmine-html-reporter'),
 			require('karma-coverage-istanbul-reporter'),
 			require('@angular-devkit/build-angular/plugins/karma')
