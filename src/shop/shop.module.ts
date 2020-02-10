88376ac5c4241647924d043d3e6036cc0e474287
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-09 21:03:53 -0700

    Display item list component in shop

--- src/shop/shop.module.ts
+++ src/shop/shop.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
+import { ItemListComponent } from './item-list/item-list.component'
 import { ShopComponent } from './shop.component'
 
 export const routes: Routes = [
@@ -10,6 +11,7 @@ export const routes: Routes = [
 
 @NgModule({
 	declarations: [
+		ItemListComponent,
 		ShopComponent
 	],
 	exports: [
