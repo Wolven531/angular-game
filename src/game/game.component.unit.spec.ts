6029ccca0b7556b16ae31de1070073cc68a1157c
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-11 21:14:46 -0700

    Enhance game unit tests
    * Test if LocStorageService.loadCoins is called

--- src/game/game.component.unit.spec.ts
+++ src/game/game.component.unit.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
+import { LocStorageService } from '@services/loc-storage.service'
+
 import { GameComponent } from './game.component'
 
 describe('GameComponent', () => {
@@ -13,14 +15,21 @@ describe('GameComponent', () => {
 	describe('when created', () => {
 		let compiled: HTMLElement
 		let fixture: ComponentFixture<GameComponent>
+		let instanceLocStorageService: LocStorageService
+		let spyLoadCoins: any
 
 		beforeEach(() => {
 			fixture = TestBed.createComponent(GameComponent)
 			compiled = fixture.debugElement.nativeElement
+			instanceLocStorageService = TestBed.get(LocStorageService)
+			spyLoadCoins = spyOn(instanceLocStorageService, 'loadCoins').and.returnValue(50)
+			fixture.detectChanges() // necessary to run ngOnInit()
 		})
 
-		it('creates game component', () => {
+		it('creates game component and invokes svc.loadCoins', () => {
 			expect(fixture.debugElement.componentInstance).toBeTruthy()
+			expect(instanceLocStorageService.loadCoins).toHaveBeenCalledTimes(1)
+			expect(spyLoadCoins).toHaveBeenCalledTimes(1)
 		})
 	 })
 })
