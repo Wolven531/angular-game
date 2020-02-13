871db8f0c9013a4ddcfd21fb04cd37489c9c7703
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-12 19:59:35 -0700

    Add func. for onPurchaseSoldier

--- src/shop/item-list/item-list.component.ts
+++ src/shop/item-list/item-list.component.ts
@@ -11,14 +11,20 @@ export class ItemListComponent implements OnInit {
 	goldBars = 0
 	numSoldiers = 0
 
-	constructor(private locStorageService: LocStorageService) {
-
-	}
-
 	ngOnInit() {
 		this.goldBars = this.locStorageService.loadGoldBars()
 	}
 
 	onPurchaseSoldier() {
+		if (this.goldBars < 5) {
+			return
+		}
+
+		this.goldBars -= 5
+		this.numSoldiers += 1
+
+		this.locStorageService.saveGoldBars(this.goldBars)
 	}
+
+	constructor(private locStorageService: LocStorageService) {}
 }
