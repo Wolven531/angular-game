53f120cab36b73d92414129c407a1f2eabcb37d8
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-20 21:43:06 -0700

    Fix missing child element, using ngIf directive

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,9 +1,8 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
-
+import { MinionComponent } from '../minion/minion.component'
 import { GameComponent } from './game.component'
-import { MinionComponent } from './minion.component'
 
 export const routes: Routes = [
 	{ path: 'game', component: GameComponent }
