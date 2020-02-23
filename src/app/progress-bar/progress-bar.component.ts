7f3257674d2681361420ddc7c3088bd4ad40ecaa
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-23 16:25:31 -0700

    Switch to div progress instead of SVG

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -2,9 +2,10 @@ import { Component, Input } from '@angular/core'
 
 @Component({
 	selector: 'ag-progress',
-	// styleUrls: ['./progress-bar.component.scss'],
+	styleUrls: ['./progress-bar.component.scss'],
 	templateUrl: './progress-bar.component.html'
 })
 export class ProgressBarComponent {
-	@Input() value = 0
+	@Input()
+	value = 0
 }
