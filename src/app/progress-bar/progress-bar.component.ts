ed01139dfbb05bc5e26187cb745275edb8317351
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-23 16:10:57 -0700

    Start progress bar component

---
+++ src/app/progress-bar/progress-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core'
+
+@Component({
+	selector: 'ag-progress',
+	// styleUrls: ['./progress-bar.component.scss'],
+	templateUrl: './progress-bar.component.html'
+})
+export class ProgressBarComponent {
+	@Input() value = 0
+}
