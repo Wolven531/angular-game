2a6b3393208114697c49b63ca2355fcab66755f1
Anthony Williams <anthony.williams.cs@gmail.com>
2020-02-22 20:29:37 -0700

    Add basic structure and style for quester

--- src/app/quester/quester.component.ts
+++ src/app/quester/quester.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core'
 
 @Component({
 	selector: 'ag-quester',
-	// styleUrls: ['./quester.component.scss'],
+	styleUrls: ['./quester.component.scss'],
 	templateUrl: './quester.component.html'
 })
 export class QuesterComponent {
