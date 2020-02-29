a28e1e84196966fc74ad8376288de74df9249c68
Wolven531 <anthony.williams.cs@gmail.com>
2020-02-28 23:11:48 -0700

    Load numSoldiers in Minion component

--- src/app/minion/minion.component.ts
+++ src/app/minion/minion.component.ts
@@ -1,11 +1,20 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { LocStorageService } from '@services/loc-storage.service'
 
 @Component({
 	selector: 'ag-minion',
 	styleUrls: ['./minion.component.scss'],
 	templateUrl: './minion.component.html'
 })
-export class MinionComponent {
+export class MinionComponent implements OnInit {
 	@Input()
 	public minion: any
+
+	public numSoldiers = 0
+
+	constructor(private readonly locStorageService: LocStorageService) {}
+
+	public ngOnInit(): void {
+		this.numSoldiers = this.locStorageService.loadNumSoldiers()
+	}
 }
