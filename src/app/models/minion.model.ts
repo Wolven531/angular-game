0148e1fabadd076b712be7993342d20fcf316180
Wolven531 <anthony.williams.cs@gmail.com>
2020-02-29 22:59:42 -0700

    Add minion model

---
+++ src/app/models/minion.model.ts
@@ -0,0 +1,11 @@
+export class Minion {
+	public static maxAttack = 10
+	public static maxDefense = 3
+	public static maxHitpoints = 5
+	public static minAttack = 1
+	public static minDefense = 1
+	public static minHitpoints = 1
+
+	constructor() {
+	}
+}
