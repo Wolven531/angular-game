1b2a82e1c6825104887f510aa0f2049260bcad4e
Wolven531 <anthony.williams.cs@gmail.com>
2020-03-01 22:12:33 -0700

    Add initialize-able ctor to Minion

--- src/app/models/minion.model.ts
+++ src/app/models/minion.model.ts
@@ -22,20 +22,28 @@ export class Minion {
 		return Math.max(this._hp - this._damageTaken, 0)
 	}
 
-	// tslint:disable-next-line: variable-name
-	private _attack: number
-	// tslint:disable-next-line: variable-name
-	private _defense: number
-	// tslint:disable-next-line: variable-name
-	private _hp: number
-	// tslint:disable-next-line: variable-name
-	private _damageTaken: number
+	constructor(
+		// tslint:disable-next-line: variable-name
+		private _attack?: number,
+		// tslint:disable-next-line: variable-name
+		private _defense?: number,
+		// tslint:disable-next-line: variable-name
+		private _hp?: number,
+		// tslint:disable-next-line: variable-name
+		private _damageTaken?: number,
+	) {
+		this._attack = _attack === undefined
+			? Minion.MIN_ATTACK + Math.round(Math.random() * (Minion.MAX_ATTACK - Minion.MIN_ATTACK))
+			: _attack
+		this._defense = _defense === undefined
+			? Minion.MIN_DEFENSE + Math.round(Math.random() * (Minion.MAX_DEFENSE - Minion.MIN_DEFENSE))
+			: _defense
+		this._hp = _hp === undefined
+			? Minion.MIN_HITPOINTS + Math.round(Math.random() * (Minion.MAX_HITPOINTS - Minion.MIN_HITPOINTS))
+			: _hp
 
-	constructor() {
-		this._attack = Minion.MIN_ATTACK + Math.round(Math.random() * (Minion.MAX_ATTACK - Minion.MIN_ATTACK))
-		this._defense = Minion.MIN_DEFENSE + Math.round(Math.random() * (Minion.MAX_DEFENSE - Minion.MIN_DEFENSE))
-		this._hp = Minion.MIN_HITPOINTS + Math.round(Math.random() * (Minion.MAX_HITPOINTS - Minion.MIN_HITPOINTS))
-
-		this._damageTaken = 0
+		this._damageTaken = _damageTaken === undefined
+			? 0
+			: _damageTaken
 	}
 }
