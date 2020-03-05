3db9c2d08d68d85fee90aa7f12d5c44ef86e4552
Anthony Williams <anthony.williams.cs@gmail.com>
2020-03-04 22:52:19 -0700

    Add eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es6': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended'
+	],
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
