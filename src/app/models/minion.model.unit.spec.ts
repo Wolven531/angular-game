0eedcd9583bd563a3dc7bebbaa6d25a32364693b
Anthony Williams <anthony.williams.cs@gmail.com>
2020-03-28 22:42:45 -0600

    Add unit test for Minion ctor w/ params

--- src/app/models/minion.model.unit.spec.ts
+++ src/app/models/minion.model.unit.spec.ts
@@ -18,7 +18,25 @@ describe('Minion', () => {
 			expect(fixture.name).toBe('')
 			expect(fixture.xp).toBe(0)
 			expect(fixture.hasTakenDamage).toBe(false)
-			expect(fixture.hitpoints).toBe(fixture.hitpointsRemaining)
+			expect(fixture.hitpointsRemaining).toBe(fixture.hitpoints)
+		})
+	})
+
+	describe('when model created using params', () => {
+		let fixture: Minion
+
+		beforeEach(() => {
+			fixture = new Minion(5, 4, 3, 2, 'name1', 1)
+		})
+
+		it('creates minion model w/ provided values', () => {
+			expect(fixture.attack).toBe(5)
+			expect(fixture.defense).toBe(4)
+			expect(fixture.hitpoints).toBe(3)
+			expect(fixture.name).toBe('name1')
+			expect(fixture.xp).toBe(1)
+			expect(fixture.hasTakenDamage).toBe(true)
+			expect(fixture.hitpointsRemaining).toBe(1)
 		})
 	})
 })
