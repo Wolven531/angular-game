c4549ccdac0ce16ad87a6cd851b4087cf27aa2fd
Anthony Williams <anthony.williams.cs@gmail.com>
2020-04-02 21:23:44 -0600

    Enhance unit test for quester

--- src/app/quester/quester.component.unit.spec.ts
+++ src/app/quester/quester.component.unit.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { Minion } from '@models/minion.model'
 import { QuesterComponent } from './quester.component'
 
 describe('QuesterComponent', () => {
@@ -10,16 +11,29 @@ describe('QuesterComponent', () => {
 	}))
 
 	describe('when created', () => {
-		// let compiled: HTMLElement
+		let compiled: HTMLElement
 		let fixture: ComponentFixture<QuesterComponent>
 
 		beforeEach(() => {
 			fixture = TestBed.createComponent(QuesterComponent)
-			// compiled = fixture.debugElement.nativeElement
+			fixture.componentInstance.minion = new Minion(5, 4, 3, 2, 'minion 1', 100)
+			compiled = fixture.debugElement.nativeElement
 		})
 
 		it('creates quester component', () => {
 			expect(fixture.debugElement.componentInstance).toBeTruthy()
+		})
+
+		describe('when changes are detected', () => {
+			beforeEach(() => {
+				fixture.detectChanges()
+			})
+
+			it('renders start, refund, and heal buttons', () => {
+				expect(compiled.querySelector('.quester-container > button.btn-start-quest').textContent).toBe('Start Quest')
+				expect(compiled.querySelector('.quester-container > button.btn-refund-minion').textContent).toBe('Refund for 75%')
+				expect(compiled.querySelector('.quester-container > button.btn-heal-minion').textContent).toBe('Heal Minion for 3 coins and 10 XP')
+			})
 		})
 	})
 })
