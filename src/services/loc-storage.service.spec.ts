16b32d69fd3c5d2e60d51f312f8d065a5c7a6319
Anthony Williams <anthony.williams.cs@gmail.com>
2020-04-10 22:39:33 -0600

    Unit test - loc storage, saveMinions filled array

--- src/services/loc-storage.service.spec.ts
+++ src/services/loc-storage.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing'
 import { LocStorageService } from './loc-storage.service'
+import { Minion } from '@models/minion.model'
 
 describe('LocStorageService', () => {
 	let fixture: LocStorageService
@@ -19,6 +20,19 @@ describe('LocStorageService', () => {
 		beforeEach(() => {
 			spySetItem = spyOn(window.localStorage, 'setItem')
 			fixture.saveMinions([])
+		})
+
+		it('uses local storage to save the empty array', () => {
+			expect(spySetItem).toHaveBeenCalledTimes(1)
+		})
+	})
+
+	describe('invoke saveMinions w/ populated array', () => {
+		let spySetItem: jasmine.Spy
+
+		beforeEach(() => {
+			spySetItem = spyOn(window.localStorage, 'setItem')
+			fixture.saveMinions([new Minion(5, 5, 5, 1, 'mon 1', 5)])
 		})
 
 		it('uses local storage to save the empty array', () => {
